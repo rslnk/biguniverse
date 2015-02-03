4dd3b391e1afc264131b17b535816a6e2196e550
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-02-03 20:12:24 +0200

    Deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,23 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'biguniverse'
+set :user, "biguniverse"
+
+set :scm, :git
+set :repo_url, "git@bitbucket.org:rslnk/biguniverse.git"
+
+# Webfaction Server (both for production and staging)
+server "#{fetch(:user)}.webfactional.com", user: "#{fetch(:user)}", roles: %w{web app db}
+
+# Webfaction specific settings
+# =====================================================
+
+# Set temp directory for username 'deploy'
+set :tmp_dir, "/home/#{fetch(:user)}/tmp"
+
+# Run composer update through SSHKit command
+# Note that composer must be installed in users home dir
+SSHKit.config.command_map[:composer] = "php55 -d memory_limit=512M -d allow_url_fopen=1 -d suhosin.executor.include.whitelist=phar /home/#{fetch(:user)}/composer/composer.phar"
+
+# =====================================================
 
 # Branch options
 # Prompts for the branch name (defaults to current branch)
@@ -9,14 +27,14 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # This could be overridden in a stage config file
 set :branch, :master
 
-set :deploy_to, -> { "/srv/www/#{fetch(:application)}" }
+#set :deploy_to, -> { "/srv/www/#{fetch(:application)}" }
 
 # Use :debug for more verbose output when troubleshooting
 set :log_level, :info
 
 # Apache users with .htaccess files:
 # it needs to be added to linked_files so it persists across deploys:
-# set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
+set :linked_files, fetch(:linked_files, []).push('.env', 'web/.htaccess')
 set :linked_files, fetch(:linked_files, []).push('.env')
 set :linked_dirs, fetch(:linked_dirs, []).push('web/app/uploads')
 
