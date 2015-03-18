c9a2e621117af39d0133e3c8ddc542ab7280500b
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-03-18 20:50:21 +0100

    Move comments to a separate tamplate file

---
+++ web/app/themes/clusteronetwo/comments.php
@@ -0,0 +1,19 @@
+<div>
+  <h1 class="head">Комментарии</h1>
+  <br />
+  <div class="split"></div>
+</div>
+
+<br /><br />
+
+<div class="split-comments">Комментарии <strong>ВКонтакте</strong></div>
+
+<div id="vk_comments"></div>
+<script type="text/javascript">
+  VK.Widgets.Comments("vk_comments", {limit: 10, width: "650", attach: "*"});
+</script>
+
+<br /><br />
+
+<div class="split-comments">Комментарии <strong>Фейсбук</strong></div>
+<div class="fb-comments" data-href="<?php echo get_permalink(); ?>" data-width="650" data-numposts="20" data-colorscheme="light"></div>
--- web/app/themes/clusteronetwo/single-photos.php
+++ web/app/themes/clusteronetwo/single-photos.php
@@ -53,23 +53,7 @@
 
 	<section id="extras-block" class="column-4x">
 
-		<div>
-			<h1 class="head">Комментарии</h1>
-			<br />
-			<div class="split"></div>
-		</div>
-
-		<br /><br />
-
-		<div class="split-comments">Комментарии <strong>ВКонтакте</strong></div>
-
-			<div id="vk_comments"></div>
-			<script type="text/javascript">
-			VK.Widgets.Comments("vk_comments", {limit: 10, width: "650", attach: "*"});
-			</script>
-			<br /><br />
-		<div class="split-comments">Комментарии <strong>Фейсбук</strong></div>
-		<div id="fb-root"></div><script src="http://connect.facebook.net/ru_RU/all.js#xfbml=1"></script><fb:comments href="<?php echo get_permalink(); ?>" num_posts="20" width="650"></fb:comments>
+		<?php get_template_part( 'comments' );?>
 
 	<?php endwhile; ?>
 
