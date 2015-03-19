8a45ea5655cb3e01a641e78d1b5f5d3b389dc429
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-03-19 15:47:12 +0100

    Rename and cleanup sidebar widgets, add twitter widget

--- web/app/themes/clusteronetwo/sidebar.php
+++ web/app/themes/clusteronetwo/sidebar.php
@@ -1,16 +1,20 @@
 <div id="sidebar" class="x2">
 
-  <?php get_template_part('widget', 'side-banner--solar-eclipse-2015'); ?>
+  <?php get_template_part('widget', 'sidebar-social--twitter'); ?>
 
-  <?php if ( !is_tag('solnechnoe-zatmenie-2015') ) get_template_part('widget', 'eclipse-2015'); ?>
+  <?php if ( is_single() ) get_template_part('widget', 'sidebar-ads--telescopes'); ?>
 
-  <?php if ( !is_tag('new-horizons') ) get_template_part('widget', 'new-horizons'); ?>
+  <?php get_template_part('widget', 'sidebar-ads--solar-eclipse-2015'); ?>
 
-	<?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'vk'); ?>
+  <?php if ( !is_tag('solnechnoe-zatmenie-2015') ) get_template_part('widget', 'sidebar-posts--solar-eclipse-2015'); ?>
 
-	<?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'facebook'); ?>
+  <?php if ( !is_tag('new-horizons') ) get_template_part('widget', 'sidebar-posts--new-horizons'); ?>
 
-	<?php if ( !is_home() && !is_category('wiki') ) get_template_part('widget', 'wiki'); ?>
+	<?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'sidebar-social--vk'); ?>
+
+	<?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'sidebar-social--facebook'); ?>
+
+	<?php if ( !is_home() && !is_category('wiki') ) get_template_part('widget', 'sidebar-wiki'); ?>
 
 
 </div>
--- web/app/themes/clusteronetwo/widget-side-banner-two.php
+++
@@ -1,3 +0,0 @@
-<div id="teaser-block">
-	<a href="<?php echo home_url(); ?>/skypatrol/otkuda-berutsya-opasnyie-asteroidyi"><img src="<?php bloginfo('template_directory') ?>/images/opasnye-asteroidy.jpg" alt="«Большая Вселенная» рассказывает откуда беруться опасные астероиды, метеориты, кометы"></img></a>
-</div>
