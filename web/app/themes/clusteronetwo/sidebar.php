c9cee789a992467da6bf813715df08ca544bd7db
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-22 16:22:54 +0200

    Reorder widgets

--- web/app/themes/clusteronetwo/sidebar.php
+++ web/app/themes/clusteronetwo/sidebar.php
@@ -2,11 +2,11 @@
 
   <?php get_template_part('widget', 'sidebar-social--twitter'); ?>
 
-  <?php if ( !is_tag('tserera') ) get_template_part('widget', 'sidebar-posts--tserera'); ?>
-
   <?php if ( !is_tag('rasskazy-o-zvezdah') ) get_template_part('widget', 'sidebar-posts--stars-stories'); ?>
 
   <?php if ( !is_tag('new-horizons') ) get_template_part('widget', 'sidebar-posts--new-horizons'); ?>
+
+  <?php if ( !is_tag('tserera') ) get_template_part('widget', 'sidebar-posts--tserera'); ?>
 
 	<?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'sidebar-social--vk'); ?>
 
