74fe924da63316586e62583894e3c4b987a3cc26
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2015-05-28 01:52:26 +0200

    Install basic mu-plugins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+### 2.8.1: 2015-05-28
+
+* Add mu-plugin Soil 3.4.0
+* Add mu-plugin wp-h5bp-htaccess 2.0.1
+* Add mu-plugin ACF Pro 5.2.5
+
 ### 2.8.0: 2015-05-27
 
 * Install Bedrock 1.3.7
---
+++ web/app/mu-plugins/base/setup.php
@@ -0,0 +1,67 @@
+<?php
+/*
+  Plugin Name:  Base Setup
+  Description:  Automatically sets site specific defaults.
+  Version:      1.0.0
+  Author:       Ruslan Komjakov
+  Author URI:   https://github.com/rslnk
+
+  * Sets upload path to /media;
+  * Sets upload path URL to example.com/media
+  * Disables year/month folder structure for uploads
+  * Sets permalink structure to post name
+  * Sets save/load path to ACF PRO JSON custom fileds
+
+*/
+
+
+if (!is_blog_installed()) { return; }
+
+/*
+
+  WordPress Setup
+  ---------------
+
+  Set uploads path to /media;
+  Set uploads URL path to example.com/media;
+  Set permalink structure to post name
+
+  Based on Roots.io wp-setup.php file
+  https://gist.github.com/swalkinshaw/6400708
+
+*/
+
+if ('http://' . $_SERVER['SERVER_NAME'] . '/wp' == get_option('siteurl')) {
+  update_option('upload_path', $_SERVER['DOCUMENT_ROOT'] . '/media');
+  update_option('upload_url_path', 'http://' . $_SERVER['SERVER_NAME'] . '/media');
+  update_option('uploads_use_yearmonth_folders', false);
+  update_option('permalink_structure', '/%postname%/');
+}
+
+/*
+
+  ACF PRO Setup
+  -------------
+
+  Set custom save/load path to JSON custom fields
+  http://www.advancedcustomfields.com/resources/local-json/
+
+*/
+
+add_filter('acf/settings/save_json', 'custom_acf_json_save_point');
+add_filter('acf/settings/load_json', 'custom_acf_json_load_point');
+
+function custom_acf_json_save_point( $path ) {
+  // update path
+  $path = __DIR__ . '/custom-fields';
+  // return
+  return $path;
+}
+function custom_acf_json_load_point( $paths ) {
+  // remove original path (optional)
+  unset($paths[0]);
+  // append path
+  $paths[] = __DIR__ . '/custom-fields';
+  // return
+  return $paths;
+}
