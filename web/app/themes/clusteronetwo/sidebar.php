54130bb9e2b9711d23dd518b87c9484d8f027007
Ruslan Komjakov <ruslan.komjakov@gmail.com>
2016-10-05 15:11:45 +0200

    Add constellation stories widget and teaser

--- web/app/themes/clusteronetwo/sidebar.php
+++ web/app/themes/clusteronetwo/sidebar.php
@@ -1,26 +1,18 @@
 <div id="sidebar" class="x2">
 
-    <?php get_template_part('widget', 'sidebar-social--twitter'); ?>
-
-    <?php //if ( !is_tag('perseidy-2016') ) get_template_part('widget', 'sidebar-posts--perseidy-2016'); ?>
-
-    <?php if ( !is_tag('gaia') ) get_template_part('widget', 'sidebar-posts--gaia'); ?>
+    <?php if ( !is_single() ) get_template_part('widget', 'widget-sidebar-teaser--bolshaya-medvedica'); ?>
 
     <?php if ( !is_tag('juno') ) get_template_part('widget', 'sidebar-posts--juno'); ?>
 
     <?php if ( !is_tag('rasskazy-o-zvezdah') ) get_template_part('widget', 'sidebar-posts--stars-stories'); ?>
 
-    <?php //if ( !is_tag('new-horizons') ) get_template_part('widget', 'sidebar-posts--new-horizons'); ?>
+    <?php if ( !is_tag('rasskazy-o-sozvezdiyah') ) get_template_part('widget', 'sidebar-posts--constellations-stories'); ?>
 
-    <?php //if ( !is_tag('kometa-67p-churyumov-gerasimenko') ) get_template_part('widget', 'sidebar-posts--churyumov-gerasimenko'); ?>
+    <?php if ( !is_search() ) get_template_part('widget', 'sidebar-social--twitter'); ?>
 
-    <?php // if ( !is_tag('kometa-c-2013-us10-catalina') ) get_template_part('widget', 'sidebar-posts--kometa-c-2013-us10-catalina'); ?>
+    <?php if ( !is_search() ) get_template_part('widget', 'sidebar-social--vk'); ?>
 
-    <?php //if ( !is_tag('tserera') ) get_template_part('widget', 'sidebar-posts--tserera'); ?>
-
-    <?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'sidebar-social--vk'); ?>
-
-    <?php if ( !is_home() && !is_search() && !is_category('authors') ) get_template_part('widget', 'sidebar-social--facebook'); ?>
+    <?php if ( !is_search() ) get_template_part('widget', 'sidebar-social--facebook'); ?>
 
     <?php if ( !is_home() && !is_category('wiki') ) get_template_part('widget', 'sidebar-wiki'); ?>
 
